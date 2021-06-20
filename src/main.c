9ad70483c578574d384b1eb4ef8133a34a054396
minh-p <minhisverynormal@gmail.com>
2021-06-19 21:44:45 -0700

    Wrote introduceTurn function

--- src/main.c
+++ src/main.c
@@ -6,9 +6,34 @@
 
 #include <stdio.h>
 #include "../include/BoardBuilder.h"
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void introduceTurn(int roundPos) {
+    if (roundPos == 0) {
+        printf("Player1's turn:");
+        return;
+    }
+
+    if (roundPos % 2 == 0) {
+        printf("Player2's turn: ");
+    } else {
+        printf("Player1's turn: ");
+    }
+}
+
+void runGameLoop(char marks[]) {
+    for (int roundPos = 0; roundPos < 9; roundPos++) {
+        introduceTurn(roundPos);
+    }
+}
 
 int main() {
-    char marks[] = "__________";
+    char marks[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    // First board setup.
     buildBoard(marks);
+    // Call game loop.
+    runGameLoop(marks);
     return 0;
 }
